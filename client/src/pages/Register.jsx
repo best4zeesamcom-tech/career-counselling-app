8e63b9e9da23a920953fc6b0d5a10fe006b4e638
Muhammad Samiullah <best4zeesam.com@gmail.com>
2026-04-06 22:30:10 +0700

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-
+import API_URL from '../config';
 function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
