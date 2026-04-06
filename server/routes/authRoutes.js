ad43e17d0d42c9c08e62a859053f3efdf69862ab
Muhammad Samiullah <best4zeesam.com@gmail.com>
2026-04-06 22:52:59 +0700

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/me', auth, authController.getMe);  // Add this line
+router.get('/me', auth, authController.getMe);
 router.put('/update', auth, authController.updateProfile);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
 module.exports = router;
